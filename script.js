309f6ae200f504f386f938e8d8840f2770f28aca
Marieke Thomas <marieke.thomas@post.harvard.edu>
2018-07-18 03:31:44 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//CHALLENGE 1: Create a function that dislays on the webpage the user's breakfast in ALL CAPS when the user presses "Yell Breakfast"
+
+
+
+
+//CHALLENGE 2: Create a function that dislays the user's breakfast in all lowercase letters AND a smaller font when the user presses "Whisper Breakfast".
+
+
+
+
+//CHALLENGE 3: Create a function that dislays on the webpage the user's breakfast with the words "and bacon" when the user presses "Add Bacon." A picture of cripsy delicious bacon should also appear below the text. The picture should disappear when the user clicks another button.
+
+
+
+
+
+//CHALLENGE 4: Using the functions that you've created, write some code so that the website displays the user's breakfast in all caps with the words "AND BACON", and a picture of bacon, when the user clicks "Yell Bacon."
+
+
+
+
+//CHALLENGE 5: Using the functions that you've created, write some code so that the website displays the user's breakfast in all lowercase with the words "and bacon", and a picture of bacon, when the user clicks "Whisper Bacon."
+
+
+
+
+//CHALLENGE 6: If the user enters the word "cereal", then display on the webpage "YOU HAD A SUPER AWESOME BREAKFAST!!!!" when the user presses "Judge Breakfast." Code in two other if statements for other possible user responses and a else statement for all other breakfast items.
+
+
+
+
+
+//SUPER CHALLENGE 7: Use the recipe api at https://developer.edamam.com/ to fetch a recipe name and image and display it to the user when they press "Suggest a Breakfast". YOU ARE IN SUPER SAIYAN MODE IF YOU CAN DO THIS!
